7a39b2c1a40cbf402ce46512a0c9ff76c4a070fb
Jignesh <jigs.patelit@gmail.com>
2020-05-17 13:04:18 +0530

    feat: add target for framework example

---
+++ ConsoleLogFile/ConsoleLogFile/ConsoleLogFile.h
@@ -0,0 +1,19 @@
+//
+//  ConsoleLogFile.h
+//  ConsoleLogFile
+//
+//  Created by Jignesh Patel on 17/05/20.
+//  Copyright © 2020 Jignesh Patel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConsoleLogFile.
+FOUNDATION_EXPORT double ConsoleLogFileVersionNumber;
+
+//! Project version string for ConsoleLogFile.
+FOUNDATION_EXPORT const unsigned char ConsoleLogFileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConsoleLogFile/PublicHeader.h>
+
+
---
+++ ConsoleLogFile/ConsoleLogFileExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConsoleLogFileExample
+//
+//  Created by Jignesh Patel on 17/05/20.
+//  Copyright © 2020 Jignesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
